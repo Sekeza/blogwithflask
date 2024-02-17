168e83c97f782c09ca8dc468e6e87a805e8f675c
Seyi Samuel <Seyi.samuel10@yahoo.com>
2024-02-17 20:28:34 +0000

    The about function was added to the app to which a user can route to.

--- blogwithflask/blogwithflask/users/routes.py
+++ blogwithflask/blogwithflask/users/routes.py
@@ -26,3 +26,9 @@ posts = [
 @users.route('/')
 def home():
     return render_template('home.html', posts=posts)
+
+@users.route('/about')
+def about():
+    return render_template('about.html', title='About Page')
+
+
