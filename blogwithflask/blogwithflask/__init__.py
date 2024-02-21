bf8e57c99c7275e3b57e33144844bece2852207d
Seyi Samuel <Seyi.samuel10@yahoo.com>
2024-02-21 00:45:57 +0000

    Added a app config for the secret key to the form in the init.py file

--- blogwithflask/blogwithflask/__init__.py
+++ blogwithflask/blogwithflask/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'fc5ba26e3c404a3efb44dca49e77520f'
+
 from blogwithflask.users.routes import users
 
 app.register_blueprint(users)
