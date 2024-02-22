3af2fa5308e241357d864f2ec23cc95c34e2cec3
Seyi Samuel <Seyi.samuel10@yahoo.com>
2024-02-22 23:46:00 +0000

    Installed and imported flask_bcrypt and flask_sqlalchemy for their respective objects: Bcrypt and SQLAlchemy. These objects were later imported to routes and models modules.

--- blogwithflask/blogwithflask/__init__.py
+++ blogwithflask/blogwithflask/__init__.py
@@ -1,8 +1,13 @@
 from flask import Flask
-
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'fc5ba26e3c404a3efb44dca49e77520f'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite///:site.db'
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from blogwithflask.users.routes import users
 
