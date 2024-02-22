64576667aa3a7cd67b70a340b75e7038b07da41d
Seyi Samuel <Seyi.samuel10@yahoo.com>
2024-02-22 22:53:42 +0000

    Created a login class form in a python file.

--- blogwithflask/blogwithflask/users/forms.py
+++ blogwithflask/blogwithflask/users/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class RegisterForm(FlaskForm):
@@ -7,6 +7,13 @@ class RegisterForm(FlaskForm):
     email =  StringField('Email', validators=[DataRequired(), Email(), Length(min=3, max=20)])
     password =  PasswordField('Password', validators=[DataRequired(), Length(min=3, max=20)])
     confirm_password =  PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')]) 
+    submit = SubmitField('Sign Up')   
+
+
+class LoginForm(FlaskForm):
+    email =  StringField('Email', validators=[DataRequired(), Email(), Length(min=3, max=20)])
+    password =  PasswordField('Password', validators=[DataRequired(), Length(min=3, max=20)])
+    remember = BooleanField('Remember me')
     submit = SubmitField('Sign In')   
 
 
