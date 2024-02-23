ca375b2cdc377d7e002d6968acd9e5e92e34b27f
Seyi Samuel <Seyi.samuel10@yahoo.com>
2024-02-23 00:18:36 +0000

    Edited the sqlalchemy aspect of the code

--- blogwithflask/blogwithflask/__init__.py
+++ blogwithflask/blogwithflask/__init__.py
@@ -4,7 +4,7 @@ from flask_bcrypt import Bcrypt
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'fc5ba26e3c404a3efb44dca49e77520f'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite///:site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
