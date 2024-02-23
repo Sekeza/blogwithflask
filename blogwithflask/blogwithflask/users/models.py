7aafff38c01b28353f2db05fa119ae50776b388e
Seyi Samuel <Seyi.samuel10@yahoo.com>
2024-02-23 00:21:31 +0000

    Added init function to the User class

--- blogwithflask/blogwithflask/users/models.py
+++ blogwithflask/blogwithflask/users/models.py
@@ -1,10 +1,19 @@
 from blogwithflask import db
 
-class User(db.Models):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(30), nullable=False, unique=True)
     email = db.Column(db.String(70), nullable=False, unique=True)
     image_file = db.Column(db.String(60), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
 
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = password  
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
     
+    
+
