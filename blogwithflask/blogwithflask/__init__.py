2c97aca284ffe7bee49c253a2b4089d3b406b6a6
Seyi Samuel <Seyi.samuel10@yahoo.com>
2024-02-23 00:49:57 +0000

    Added with app.app_context():

--- blogwithflask/blogwithflask/__init__.py
+++ blogwithflask/blogwithflask/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 app = Flask(__name__)
+# app.app_context().push()
 
 app.config['SECRET_KEY'] = 'fc5ba26e3c404a3efb44dca49e77520f'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
@@ -13,3 +14,5 @@ from blogwithflask.users.routes import users
 
 app.register_blueprint(users)
 
+with app.app_context():
+    db.create_all()
