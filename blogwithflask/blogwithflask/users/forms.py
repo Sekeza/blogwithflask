d26beac3314abcbc929491b5c7018e82b257a10f
Seyi Samuel <Seyi.samuel10@yahoo.com>
2024-02-24 23:09:35 +0000

    Added the right path to import the User object from the models module.

--- blogwithflask/blogwithflask/users/forms.py
+++ blogwithflask/blogwithflask/users/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
-from models import User
+from blogwithflask.users.models import User
 
 class RegisterForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=3, max=20)])
