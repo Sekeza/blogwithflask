a7d58c47b5826f36bb5da96ed273ad56f8b5e8e1
Seyi Samuel <Seyi.samuel10@yahoo.com>
2024-02-24 23:07:36 +0000

    Imported the flask login and stored the LoginManager object instance in a variable called login_manager

--- blogwithflask/blogwithflask/__init__.py
+++ blogwithflask/blogwithflask/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 app = Flask(__name__)
 app.app_context().push()
 
@@ -9,6 +10,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from blogwithflask.users.routes import users
 
