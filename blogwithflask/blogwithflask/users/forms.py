e55ab28c3ba83552ea6931ce746bed0f518f16d8
Seyi Samuel <Seyi.samuel10@yahoo.com>
2024-02-24 22:57:28 +0000

    Added two functions inside the RegisterForm class of the forms.py module. These functions will prevent duplicates of emails and usernames in the database.

--- blogwithflask/blogwithflask/users/forms.py
+++ blogwithflask/blogwithflask/users/forms.py
@@ -1,13 +1,24 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from models import User
 
 class RegisterForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=3, max=20)])
     email =  StringField('Email', validators=[DataRequired(), Email(), Length(min=3, max=20)])
     password =  PasswordField('Password', validators=[DataRequired(), Length(min=3, max=20)])
     confirm_password =  PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')]) 
-    submit = SubmitField('Sign Up')   
+    submit = SubmitField('Sign Up') 
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data) 
+        if user:
+            raise ValidationError('The username is already taken. Please choose a different one!') 
+        
+    def validate_email(self, email):
+        user = User.query.filter_by(username=email.data) 
+        if user:
+            raise ValidationError('The email is already taken. Please choose a different one!') 
 
 
 class LoginForm(FlaskForm):
