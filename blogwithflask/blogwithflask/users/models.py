f9bd22e5e467e07b68c94fa6e6013f485bcbdcfb
Seyi Samuel <Seyi.samuel10@yahoo.com>
2024-02-24 23:20:39 +0000

    Created a load_user function inside the models.py module so that the flask extension can automatically load a user details and log them in. The login_manager variable object was imported from the package and use to get the user_loader properties.

--- blogwithflask/blogwithflask/users/models.py
+++ blogwithflask/blogwithflask/users/models.py
@@ -1,4 +1,9 @@
-from blogwithflask import db
+from blogwithflask import db, login_manager
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
