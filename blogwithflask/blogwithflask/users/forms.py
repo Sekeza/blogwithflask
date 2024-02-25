03d79accf143c2e19f34b3f826093e0541d53b5e
Seyi Samuel <Seyi.samuel10@yahoo.com>
2024-02-25 01:34:00 +0000

    The updateAccountForm class was added to the forms.py file

--- blogwithflask/blogwithflask/users/forms.py
+++ blogwithflask/blogwithflask/users/forms.py
@@ -1,4 +1,6 @@
 from flask_wtf import FlaskForm
+from flask_login import current_user
+from flask_wtf.file import FileField, FileAllowed, FileRequired
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from blogwithflask.users.models import User
@@ -27,5 +29,23 @@ class LoginForm(FlaskForm):
     remember = BooleanField('Remember me')
     submit = SubmitField('Sign In')   
 
+
+class updateAccountForm(FlaskForm):
+    username = StringField( 'Username', validators=[DataRequired(), Length(min=3, max=20)] )
+    email = StringField( 'Email', validators=[ DataRequired(), Email() ] )
+    picture = FileField('Update Profile Picture', validators=[ FileRequired(), FileAllowed(['png', 'jpg'], "Wrong format!") ] )
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username has been taken, Please choose a different one!')
+        
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('That email has been taken, Please choose a different one!')
 
 
