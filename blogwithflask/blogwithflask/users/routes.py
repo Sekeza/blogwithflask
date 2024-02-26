08025f2fb03dc0d5f42b7a529daf001b4ae7f219
Seyi Samuel <Seyi.samuel10@yahoo.com>
2024-02-26 04:31:00 +0000

    Imported and added the login_required object from flask_login.

--- blogwithflask/blogwithflask/users/routes.py
+++ blogwithflask/blogwithflask/users/routes.py
@@ -5,7 +5,7 @@ import email_validator
 from blogwithflask import bcrypt, db, app
 from blogwithflask.users.forms import RegisterForm, LoginForm, updateAccountForm
 from blogwithflask.users.models import User
-from flask_login import login_user, current_user, logout_user
+from flask_login import login_user, current_user, login_required, logout_user
 
 users = Blueprint('users', __name__)
 
@@ -49,7 +49,7 @@ def register():
         user = User(username=form.username.data, email=form.email.data, password=hash_passwd)
         db.session.add(user)
         db.session.commit()
-        flash(f'Account created for {form.username.data}! You can now log in.', 'success')
+        flash('Account created for user! You can now log in.', 'success')
         return redirect(url_for('users.login'))
     return render_template('register.html', title='Register Page', form=form)
 
@@ -62,10 +62,9 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
-            flash(f'Welcome {form.email.data}! You can log in.', 'success')
+            flash('Welcome! You can log in.', 'success')
             login_user(user, remember=form.remember.data)
-            return redirect(url_for('users.home'))
-            
+            return redirect(url_for('users.home'))    
         else:
             flash('Login Unsuccessful. Please check email and password', 'danger')
             return redirect(url_for('users.login'))
@@ -92,6 +91,7 @@ def save_picture(form_picture):
     return picture_filename
 
 @users.route('/account')
+@login_required
 def account():
     form = updateAccountForm()
     if form.validate_on_submit():
