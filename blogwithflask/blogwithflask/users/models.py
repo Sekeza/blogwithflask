6e5f8247e21af87e14d8e017fa8ce72510de4b67
Seyi Samuel <Seyi.samuel10@yahoo.com>
2024-02-27 23:27:52 +0000

    Created a Post class that will contain the field model such as title, content, date posted by a user. Then the db relationship is extended from the User class to the Post class so that each post is dynamically assign to the user.

--- blogwithflask/blogwithflask/users/models.py
+++ blogwithflask/blogwithflask/users/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from blogwithflask import db, login_manager
 from flask_login import UserMixin
 
@@ -12,6 +13,7 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(70), nullable=False, unique=True)
     image_file = db.Column(db.String(60), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
 
     def __init__(self, username, email, password):
         self.username = username
@@ -22,4 +24,13 @@ class User(db.Model, UserMixin):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
     
     
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
 
