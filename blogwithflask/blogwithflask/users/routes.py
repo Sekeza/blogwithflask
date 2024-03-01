c72c86ab602897d8b4c61682b449b268bc5a3c3a
Seyi Samuel <Seyi.samuel10@yahoo.com>
2024-03-01 00:31:55 +0000

    Removed a list of dictionaries called posts. This is to enable posts come directly from the database after users entered these data o the form page.

--- blogwithflask/blogwithflask/users/routes.py
+++ blogwithflask/blogwithflask/users/routes.py
@@ -9,26 +9,7 @@ from flask_login import login_user, current_user, login_required, logout_user
 
 users = Blueprint('users', __name__)
 
-posts = [
-    {
-        'title': 'First WebApp Post',
-        'content': 'Finally getting to produce content with backend and frontend seamlessly',
-        'author': 'Zane',
-        'date_posted': 'February 14, 2024',
-    },
-    {
-        'title': 'Life as we see',
-        'content': 'take a bold step to get there in time',
-        'author': 'Kay',
-        'date_posted': 'February 15, 2024',
-    },
-    {
-        'title': 'Weather storm and helicopter crashing',
-        'content': 'Investigations into the frequent occurrence of air mishap in the North West of America',
-        'author': 'Shey',
-        'date_posted': 'February 16, 2024',
-    }
-]
+
 
 @users.route('/')
 def home():
