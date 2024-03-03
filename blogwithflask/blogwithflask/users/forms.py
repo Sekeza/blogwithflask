3a73ee2ffd0a8888712ea156fab5401532f731b1
Seyi Samuel <Seyi.samuel10@yahoo.com>
2024-03-03 23:17:29 +0000

    A class named RequestResetForm is created  that have email and submit fields for user to access a token.

--- blogwithflask/blogwithflask/users/forms.py
+++ blogwithflask/blogwithflask/users/forms.py
@@ -55,4 +55,12 @@ class PostForm(FlaskForm):
     submit = SubmitField('Post') 
 
 
+class RequestResetForm(FlaskForm):
+    email = StringField( 'Email', validators=[DataRequired(), Email()] )
+    submit = SubmitField('Request Password Reset')
+        
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is None:
+            raise ValidationError('There is no account with that email, Please register first!')
 
