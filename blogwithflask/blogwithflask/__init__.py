b0a778981198482a3a2451e8b35598674a647f45
Seyi Samuel <Seyi.samuel10@yahoo.com>
2024-03-03 23:43:22 +0000

    Installed flask-mail extension and imported it to the python package file. Then created a configuration for the port entry, server, and ids for username and password

--- blogwithflask/blogwithflask/__init__.py
+++ blogwithflask/blogwithflask/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 app = Flask(__name__)
 app.app_context().push()
 
@@ -13,6 +14,12 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'syglemelg@gmail.com' #os.environ.get('MAIL_USERNAME') 
+app.config['MAIL_PASSWORD'] = 'shakingmyhead' #os.environ.get('MAIL_PASSWORD') 
+mail = Mail(app)
 
 from blogwithflask.users.routes import users
 
