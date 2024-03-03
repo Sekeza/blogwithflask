1153214fb74ea497f8e01a5da2873fb71da973c5
Seyi Samuel <Seyi.samuel10@yahoo.com>
2024-03-03 23:02:42 +0000

    Created verify_reset_token function that will validate the token generated from get_reset_token function in the User class.

--- blogwithflask/blogwithflask/users/models.py
+++ blogwithflask/blogwithflask/users/models.py
@@ -29,6 +29,15 @@ class User(db.Model, UserMixin):
         s = Serializer(app.config['SECRET_KEY'], expires_sec)
         return s.dumps({'user_id': self.id}).decode('utf-8')
     
+    @staticmethod
+    def verify_reset_token(token):
+        s = Serializer(app.config['SECRET_KEY'])
+        try:
+            user_id = s.loads(token)['user_id']
+        except:
+            return None
+        return User.query.get(user_id)
+    
     
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
